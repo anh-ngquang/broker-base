4538bab3e5b0d8b4ec5a7dfb534acbdfc80df608
Nguyen Quang Anh <anhnq1@vtc.vn>
2018-10-29 17:22:27 +0700

    initial commit

---
+++ server/routes/account.js
@@ -0,0 +1,132 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var User = require('../../models/User.js');
+var UserSession = require('../../models/UserSession.js');
+
+router.get('/verify', function(req, res, next) {
+  // Get the token
+  const { query } = req;
+  const { token } = query;
+  // ?token=test
+  // Verify the token is one of a kind and it's not deleted.
+  UserSession.find({
+    _id: token,
+    isDeleted: false
+  }, (err, sessions) => {
+    if (err) {
+      console.log(err);
+      return res.send({
+        success: false,
+        message: 'Error: Server error'
+      });
+    }
+    if (sessions.length != 1) {
+      return res.send({
+        success: false,
+        message: 'Error: Invalid'
+      });
+    } else {
+      // DO ACTION
+      return res.send({
+        success: true,
+        message: 'Good'
+      });
+    }
+  });
+});
+
+router.post('/signin', function(req, res, next) {
+  const { body } = req;
+  const {
+    password
+  } = body;
+  let {
+    username
+  } = body;
+  if (!username) {
+    return res.send({
+      success: false,
+      message: 'Error: Username cannot be blank.'
+    });
+  }
+  if (!password) {
+    return res.send({
+      success: false,
+      message: 'Error: Password cannot be blank.'
+    });
+  }
+
+  username = username.trim();
+  User.find({
+      username: username
+  }, (err, users) => {
+    if (err) {
+      console.log('err 2:', err);
+      return res.send({
+        success: false,
+        message: 'Error: server error'
+      });
+    }
+    if (users.length != 1) {
+      return res.send({
+        success: false,
+        message: 'Error: Invalid'
+      });
+    }
+    const user = users[0];
+    if (!user.validPassword(password)) {
+      return res.send({
+        success: false,
+        message: 'Error: Invalid'
+      });
+    }
+    // Otherwise correct user
+    const userSession = new UserSession();
+    userSession.userId = user._id;
+    userSession.save((err, doc) => {
+      if (err) {
+        console.log(err);
+        return res.send({
+          success: false,
+          message: 'Error: server error'
+        });
+      }
+      return res.send({
+        success: true,
+        message: 'Valid sign in',
+        token: doc._id
+      });
+    });
+  });
+});
+
+router.get('/signout', function(req, res, next) {
+  // Get the token
+  const { query } = req;
+  const { token } = query;
+  // ?token=test
+  // Verify the token is one of a kind and it's not deleted.
+  UserSession.findOneAndUpdate({
+    _id: token,
+    isDeleted: false
+  }, {
+    $set: {
+      isDeleted:true
+    }
+  }, null, (err, sessions) => {
+    if (err) {
+      console.log(err);
+      return res.send({
+        success: false,
+        message: 'Error: Server error'
+      });
+    }
+    return res.send({
+      success: true,
+      message: 'Good'
+    });
+  });
+});
+
+module.exports = router;
---
+++ server/server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = require('./routes/routes.js')
+var path = require('path');
+
+var broker = require('./routes/broker');
+var account = require('./routes/account');
+
+var app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../client'));
+app.use(express.static(path.join(__dirname, '../client')));
+app.use('/', router);
+
+app.use('/api/broker', broker);
+app.use('/api/account', account);
+
+var mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+mongoose.connect('mongodb://localhost/broker-base', { useNewUrlParser: true, promiseLibrary: require('bluebird') })
+  .then(() =>  console.log('connection succesful'))
+  .catch((err) => console.error(err));
+
+module.exports=app;
