30e0a172c8e57f636f3d679d22bd5d490ff348e7
Nguyen Quang Anh <anhnq1@vtc.vn>
2018-11-06 17:37:32 +0700

    - Import broker (backend)

--- server/routes/broker.js
+++ server/routes/broker.js
@@ -3,12 +3,12 @@ var router = express.Router();
 var Broker = require('../../models/Broker.js');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('Express REST API');
 });
 
 // Batch import site's broker data into DB
-router.post('/import', function(req, res, next) {
+router.post('/import', async function (req, res, next) {
   const { body } = req;
   const {
     siteName,
@@ -16,49 +16,157 @@ router.post('/import', function(req, res, next) {
     batchCode
   } = body;
 
-  if (rowsData.length > 0) {
-    var importBroker = new Broker();
-    try {
-      importBroker.fromSiteExelRow(siteName, rowsData[5]);
-      console.log("Import Broker: ", importBroker);
-    } catch (e) {
-      console.log(e);
-    }
-  }
+  // if (rowsData.length > 0) {
+  //   var importBroker = new Broker();
+  //   try {
+  //     importBroker.fromSiteExelRow(siteName, rowsData[5]);
+  //     console.log("Import Broker: ", importBroker);
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // }
 
   var importedRowCount = 0;
   var currentRow = 0;
-  
+  var importError;
+
   // for each row:
+  for (var rowData of rowsData) {
+    currentRow++;
 
-  // convert rowData to Broker model
+    // convert rowData to Broker model
+    var importBroker = new Broker();
+    importBroker.import_batch_code = batchCode;
+    try {
+      importBroker.fromSiteExelRow(siteName, rowData);
+    } catch (e) {
+      console.log(e);
+      continue;
+    }
 
-  // check db for existing record based on Phone number
+    // check db for existing record based on Phone number
+    var dbBrokers;
+    await Broker.find({ phone: importBroker.phone }, function (err, docs) {
+      if (err) {
+        importError = err;
+        return;
+      }
 
-    // if not exists
+      dbBrokers = docs;
+    });
+
+    if (importError) break;
+
+    if (!dbBrokers || dbBrokers.length == 0) {
+      // if not exists
       // INSERT NEW RECORD
+      importBroker.created_date = new Date();
+      importBroker.save(function (err) {
+        if (err) {
+          importError = err;
+          return;
+        }
+      });
+      if (importError) break;
 
-      // response
-
-    // if exists
+    } else {
+      // if exists
       // get Broker model from db
+      var dbBroker = dbBrokers[0];
 
       // update imported model info into current model
-      // check for same source and import_batch_code => if true: do not update Properties
-      // areas + lowercase
-      // properties + lowercase
-      // sources
-      // updated_date
-      // import_batch_code
+      // do not update: name, phone
+
+      if (dbBroker.email == '') {
+        dbBroker.email = importBroker.email;
+      }
+
+      if (dbBroker.address == '') {
+        dbBroker.address = importBroker.address;
+        dbBroker.address_lowercase = importBroker.address_lowercase;
+      }
+
+      if (importBroker.areas && importBroker.areas.length > 0 && !dbBroker.areas.includes(importBroker.areas[0])) {
+        dbBroker.areas.push(importBroker.areas[0]);
+        dbBroker.areas_lowercase.push(importBroker.areas_lowercase[0]);
+      }
+
+      if (dbBroker.import_batch_code != importBroker.import_batch_code) {
+        // different import_batch_code => clear all current Properties and insert again
+        dbBroker.sell_properties = [];
+        dbBroker.sell_properties_lowercase = [];
+        dbBroker.rent_properties = [];
+        dbBroker.rent_properties_lowercase = [];
+        dbBroker.transfer_properties = [];
+        dbBroker.transfer_properties_lowercase = [];
+        dbBroker.other_properties = [];
+        dbBroker.other_properties_lowercase = [];
+        dbBroker.all_properties = [];
+        dbBroker.all_properties_lowercase = [];
+      }
+
+      for (var property of importBroker.sell_properties) {
+        dbBroker.sell_properties.push(property);
+      }
+      for (var property of importBroker.sell_properties_lowercase) {
+        dbBroker.sell_properties_lowercase.push(property);
+      }
+      for (var property of importBroker.rent_properties) {
+        dbBroker.rent_properties.push(property);
+      }
+      for (var property of importBroker.rent_properties_lowercase) {
+        dbBroker.rent_properties_lowercase.push(property);
+      }
+      for (var property of importBroker.transfer_properties) {
+        dbBroker.transfer_properties.push(property);
+      }
+      for (var property of importBroker.transfer_properties_lowercase) {
+        dbBroker.transfer_properties_lowercase.push(property);
+      }
+      for (var property of importBroker.other_properties) {
+        dbBroker.other_properties.push(property);
+      }
+      for (var property of importBroker.other_properties_lowercase) {
+        dbBroker.other_properties_lowercase.push(property);
+      }
+      for (var property of importBroker.all_properties) {
+        dbBroker.all_properties.push(property);
+      }
+      for (var property of importBroker.all_properties_lowercase) {
+        dbBroker.all_properties_lowercase.push(property);
+      }
+
+      if (importBroker.sources && importBroker.sources.length > 0 && !dbBroker.sources.includes(importBroker.sources[0])) {
+        dbBroker.sources.push(importBroker.sources[0]);
+      }
+
+      dbBroker.updated_date = new Date();
+      dbBroker.import_batch_code = importBroker.import_batch_code;
 
       // UPDATE DB
-
-      // response
-  
-      return res.send({
-        success: true,
-        totalUploadedRows: rowsData.length
+      Broker.update({ _id: dbBroker._id }, dbBroker, function (err, result) {
+        if (err) {
+          importError = err;
+          return;
+        }
       });
+      if (importError) break;
+    }
+
+    importedRowCount++;
+  }
+  // response
+  if (importError) {
+    return res.send({
+      success: false,
+      error: importError
+    });
+  }
+
+  return res.send({
+    success: true,
+    totalUploadedRows: importedRowCount
+  });
 });
 
 module.exports = router;
